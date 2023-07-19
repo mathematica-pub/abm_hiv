2c37d6201737bc8130af5ce54138ec11b2b68ad0
Ravi Goyal <ravigoyal@Ravis-MacBook-Pro.local>
2023-07-19 14:46:08 -0700

    commenting out testing lines

--- abm_hiv_commandline.R
+++ abm_hiv_commandline.R
@@ -7,8 +7,8 @@ file_loc_link <- args[3]
 
 file_loc_source = "./modules"
 #file_loc_input = "/Users/ravigoyal/Dropbox/Academic/Research/Projects/HRSA_SanDiego_modeling/RWHAP_Equity-feat-add_equity_outcomes/inputs_2019/user_inputs_Current_RWHAP - 200K - PrEP.xlsx"
-file_loc_input = "/Users/ravigoyal/Dropbox/Academic/Research/Projects/HRSA_SanDiego_modeling/SD_data/data.xlsx"
-file_loc_link = "/Users/ravigoyal/Dropbox/Academic/Research/Projects/HRSA_SanDiego_modeling/SD_data/demographics.csv"
+#file_loc_input = "/Users/ravigoyal/Dropbox/Academic/Research/Projects/HRSA_SanDiego_modeling/SD_data/data.xlsx"
+#file_loc_link = "/Users/ravigoyal/Dropbox/Academic/Research/Projects/HRSA_SanDiego_modeling/SD_data/demographics.csv"
 
 library(gtools)
 library(ensurer)
